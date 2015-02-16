caaf36b8f24220d3ab1964b00991a5e8d6507791
Kazushi Hara <hara.kazushi@po.ntts.co.jp>
2015-02-16 16:45:12 +0900

    署名を修正

--- RealmSample/Group.swift
+++ RealmSample/Group.swift
@@ -2,7 +2,6 @@
 //  Group.swift
 //  RealmSample
 //
-//  Created by MTER on 2015/02/16.
 //  Copyright (c) 2015年 haranicle. All rights reserved.
 //
 
--- RealmSample/Task.swift
+++ RealmSample/Task.swift
@@ -2,7 +2,6 @@
 //  Task.swift
 //  RealmSample
 //
-//  Created by MTER on 2015/02/16.
 //  Copyright (c) 2015年 haranicle. All rights reserved.
 //
 
